eb6331c717cf2fcb8bba7e79137a31033c1b23c4
Shivani849 <shivani.mandoddi@xcubelabs.com>
2022-04-05 17:54:48 +0530

    Player Spawing Positions

---
+++ Assets/PlayerSpawn.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform playerSpawnPoints;   // The parent of the spawn points
+     Transform[] spawnPositions;
+    public bool isRespawn = false;
+    public bool respawnToggle = false; 
+    void Start()
+    {
+        spawnPositions = playerSpawnPoints.GetComponentsInChildren<Transform>();
+        //RandomSpawn();
+        //Debug.Log(spawnPositions.Length);
+    }
+
+    public void RandomSpawn()
+    {
+        int i = UnityEngine.Random.Range(1, spawnPositions.Length);
+
+        Debug.Log(spawnPositions[i].transform.position);
+        this.transform.position = spawnPositions[i].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(respawnToggle!=isRespawn)
+        {
+            RandomSpawn();
+            isRespawn = false;
+        }
+        else
+        {
+            respawnToggle = isRespawn;
+        }*/
+       if(Input.GetKeyDown(KeyCode.Space))
+        {
+            RandomSpawn();
+        }
+    }
+}
